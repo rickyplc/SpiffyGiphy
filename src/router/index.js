9eef3dba6cfcf30f59a3673fce30b91c5c27b272
Ricky <Ricky@ricky.com>
2018-09-03 21:25:15 +0100

    Update routes
    - Add a route for the main root path using the trending component
    - Add a route for search using the search component

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Trending from '@/components/Trending'
+import Search from '@/components/Search'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [{
+    path: '/',
+    name: 'Trending',
+    component: Trending
+  },
+  {
+    path: '/search',
+    component: Search,
+    props: (route) => ({
+      query: route.query.q
+    })
+  }]
 })
